04c56ec4c60721b0bbd268d215ddf28d9255e67b
jt <jt>
2020-04-08 13:09:51 +0200

    formatting

--- VectorsAndMemory/src/Vectors and Memory.cpp
+++ VectorsAndMemory/src/Vectors and Memory.cpp
@@ -8,32 +8,33 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
 
-	vector<double> numbers(0);
+    vector<double> numbers(0);
 
-	cout << "Size: " << numbers.size() << endl;
+    cout << "Size: " << numbers.size() << endl;
 
-	int capacity = numbers.capacity();
-	cout << "Capacity: " << capacity << endl;
+    int capacity = numbers.capacity();
+    cout << "Capacity: " << capacity << endl;
 
-	for(int i=0; i<10000; i++) {
+    for (int i = 0; i < 10000; i++) {
 
-		if(numbers.capacity() != capacity) {
-			capacity = numbers.capacity();
-			cout << "Capacity: " << capacity << endl;
-		}
+        if (numbers.capacity() != capacity) {
+            capacity = numbers.capacity();
+            cout << "Capacity: " << capacity << endl;
+        }
 
-		numbers.push_back(i);
-	}
+        numbers.push_back(i);
+    }
 
-	numbers.reserve(100000);
-	cout << numbers[2] << endl;
-	cout << "Size: " << numbers.size() << endl;
-	cout << "Capacity: " << numbers.capacity() << endl;
+    numbers.reserve(100000);
+    cout << numbers[2] << endl;
+    cout << "Size: " << numbers.size() << endl;
+    cout << "Capacity: " << numbers.capacity() << endl;
 
 
-	return 0;
+    return 0;
 }

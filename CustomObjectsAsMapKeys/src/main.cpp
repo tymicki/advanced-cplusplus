67292ec146d1687a50cb738ef695ad1c517dca16
jt <jt>
2020-04-08 16:20:10 +0200

    set

--- CustomObjectsAsMapKeys/src/main.cpp
+++ CustomObjectsAsMapKeys/src/main.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
-#include <map>
+#include <set>
 
 using namespace std;
 
+class Test {
+    int id;
+    string name;
+
+public:
+    Test() : id(0), name("") {
+
+    }
+
+    Test(int id, string name) : id(id), name(name) {
+
+    }
+
+    void print() const {
+        cout << id << ": " << name << endl;
+    }
+
+    bool operator<(const Test &other) const {
+        return name < other.name;
+    }
+};
+
 int main() {
-    multimap<int, string> lookup;
-    lookup.insert(make_pair(30, "Mike"));
-    lookup.insert(make_pair(10, "Vicky"));
-    lookup.insert(make_pair(30, "Raj"));
-    lookup.insert(make_pair(20, "Bob"));
 
-    for (multimap<int, string>::iterator it = lookup.begin(); it != lookup.end(); ++it) {
-        cout << it->first << ": " << it->second << endl;
+    set<int> numbers;
+
+    numbers.insert(50);
+    numbers.insert(10);
+    numbers.insert(30);
+    numbers.insert(20);
+
+    for (set<int>::iterator it = numbers.begin(); it != numbers.end(); it++) {
+        cout << *it << endl;
     }
 
-    cout << endl;
-    for (multimap<int, string>::iterator it = lookup.find(20); it != lookup.end(); ++it) {
-        cout << it->first << ": " << it->second << endl;
+    set<int>::iterator itFind = numbers.find(30);
+    if (itFind != numbers.end()) {
+        cout << "Found: " << *itFind << endl;
     }
 
-    cout << endl;
-    pair<multimap<int, string>::iterator, multimap<int, string>::iterator> its = lookup.equal_range(30);
-
-    for (multimap<int, string>::iterator it = its.first; it != its.second; ++it) {
-        cout << it->first << ": " << it->second << endl;
+    if (numbers.count(30)) {
+        cout << "Number found." << endl;
     }
 
-    cout << endl;
-    auto its2 = lookup.equal_range(30);
+    set<Test> tests;
+    tests.insert(Test(10, "Mike"));
+    tests.insert(Test(30, "Joe"));
+    tests.insert(Test(3333, "Joe"));
+    tests.insert(Test(20, "Sue"));
 
-    for (auto it = its.first; it != its2.second; ++it) {
-        cout << it->first << ": " << it->second << endl;
+    for (set<Test>::iterator it = tests.begin(); it != tests.end(); it++) {
+        it->print();
     }
 
     return 0;

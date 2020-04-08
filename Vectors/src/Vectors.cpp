637fea3f789530e9f317e7461879821d4aed21e1
jt <jt>
2020-04-08 12:36:13 +0200

    formatting

--- Vectors/src/Vectors.cpp
+++ Vectors/src/Vectors.cpp
@@ -8,35 +8,35 @@
 
 #include <iostream>
 #include <vector>
-#include <string> 
+#include <string>
 
 using namespace std;
 
 int main() {
 
-	vector<string> strings;
+    vector<string> strings;
 
-	strings.push_back("one");
-	strings.push_back("two");
-	strings.push_back("three");
+    strings.push_back("one");
+    strings.push_back("two");
+    strings.push_back("three");
 
-	cout << "For loop: " << endl;
-	for(int i=0; i<strings.size(); i++) {
-		cout << strings[i] << endl;
-	}
-	cout << endl;
+    cout << "For loop: " << endl;
+    for (int i = 0; i < strings.size(); i++) {
+        cout << strings[i] << endl;
+    }
+    cout << endl;
 
-	cout << "Iterator loop: " << endl;
-	for(auto it = strings.begin(); it != strings.end(); ++it) {
-		cout << *it << endl;
-	}
-	cout << endl;
+    cout << "Iterator loop: " << endl;
+    for (auto it = strings.begin(); it != strings.end(); ++it) {
+        cout << *it << endl;
+    }
+    cout << endl;
 
-	cout << "Single item." << endl;
-	vector<string>::iterator it = strings.begin();
-	it += 2;
+    cout << "Single item." << endl;
+    vector<string>::iterator it = strings.begin();
+    it += 2;
 
-	cout << *it << endl;
+    cout << *it << endl;
 
-	return 0;
+    return 0;
 }

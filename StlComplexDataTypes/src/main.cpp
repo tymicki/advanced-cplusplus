f0dd1b781e64859210499fb505bb80ba314f8461
jt <jt>
2020-04-08 17:26:11 +0200

    format

--- StlComplexDataTypes/src/main.cpp
+++ StlComplexDataTypes/src/main.cpp
@@ -27,25 +27,23 @@ using namespace std;
 
 int main() {
 
-	map<string, vector<int> > scores;
+    map<string, vector<int> > scores;
 
-	scores["Mike"].push_back(10);
-	scores["Mike"].push_back(20);
-	scores["Vicky"].push_back(15);
+    scores["Mike"].push_back(10);
+    scores["Mike"].push_back(20);
+    scores["Vicky"].push_back(15);
 
-	for(map<string, vector<int> >::iterator it = scores.begin(); it!= scores.end(); it++)
-	{
-		string name = it->first;
-		vector<int> scoreList = it->second;
+    for (map<string, vector<int> >::iterator it = scores.begin(); it != scores.end(); it++) {
+        string name = it->first;
+        vector<int> scoreList = it->second;
 
-		cout << name << ": " << flush;
+        cout << name << ": " << flush;
 
-		for(int i = 0; i < scoreList.size(); i++)
-		{
-			cout << scoreList[i] << " " << flush;
-		}
+        for (int i = 0; i < scoreList.size(); i++) {
+            cout << scoreList[i] << " " << flush;
+        }
 
-		cout << endl;
-	}
-	return 0;
+        cout << endl;
+    }
+    return 0;
 }

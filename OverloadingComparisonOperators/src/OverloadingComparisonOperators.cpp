c7670964be530c536df1a8a7fc543dd8f4933e13
jt <jt>
2020-04-09 10:12:40 +0200

    format

--- OverloadingComparisonOperators/src/OverloadingComparisonOperators.cpp
+++ OverloadingComparisonOperators/src/OverloadingComparisonOperators.cpp
@@ -8,25 +8,26 @@
 
 #include <iostream>
 #include "Complex.h"
+
 using namespace std;
 using namespace caveofprogramming;
 
 int main() {
 
-	Complex c1(3, 2);
-	Complex c2(3, 3);
+    Complex c1(3, 2);
+    Complex c2(3, 3);
 
-	if (c1 == c2) {
-		cout << "Equal" << endl;
-	} else {
-		cout << "Not equal" << endl;
-	}
+    if (c1 == c2) {
+        cout << "Equal" << endl;
+    } else {
+        cout << "Not equal" << endl;
+    }
 
-	if (c1 != c2) {
-		cout << "Not equal" << endl;
-	} else {
-		cout << "Equal" << endl;
-	}
+    if (c1 != c2) {
+        cout << "Not equal" << endl;
+    } else {
+        cout << "Equal" << endl;
+    }
 
-	return 0;
+    return 0;
 }

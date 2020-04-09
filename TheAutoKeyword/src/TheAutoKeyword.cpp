d5106c2766a197c950fd5aeb50e2bc5282325544
jt <jt>
2020-04-09 12:38:50 +0200

    format

--- TheAutoKeyword/src/TheAutoKeyword.cpp
+++ TheAutoKeyword/src/TheAutoKeyword.cpp
@@ -7,30 +7,29 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
 
 template<class T, class S>
 auto test(T value1, S value2) -> decltype(value1 + value2) {
-	return value1 + value2;
+    return value1 + value2;
 }
 
-int get()
-{
-	return 999;
+int get() {
+    return 999;
 }
 
-auto test2() -> decltype(get())
-{
-	return get();
+auto test2() -> decltype(get()) {
+    return get();
 }
 
 int main() {
-	auto value = 7;
-	auto text = "Hello";
+    auto value = 7;
+    auto text = "Hello";
 
-	cout << test(5, 6) << endl;
-	
-	cout << test2() << endl;
+    cout << test(5, 6) << endl;
 
-	return 0;
+    cout << test2() << endl;
+
+    return 0;
 }

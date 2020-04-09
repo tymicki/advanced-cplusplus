f9dd024ed8e5e8ab8598a313730ec3f355fda89e
jt <jt>
2020-04-09 14:55:09 +0200

    format

--- InitializationInC98/src/main.cpp
+++ InitializationInC98/src/main.cpp
@@ -12,42 +12,42 @@
 using namespace std;
 
 int main() {
-	int values[] = { 1, 4, 5 };
+    int values[] = {1, 4, 5};
 
-	cout << values[0] << endl;
+    cout << values[0] << endl;
 
-	class C {
-	public:
-		string text;
-		int id;
-	};
+    class C {
+    public:
+        string text;
+        int id;
+    };
 
-	C c1 = { "Hello", 7 };
+    C c1 = {"Hello", 7};
 
-	cout << c1.text << endl;
+    cout << c1.text << endl;
 
-	struct S {
-		string text;
-		int id;
-	};
+    struct S {
+        string text;
+        int id;
+    };
 
-	S s1 = { "Hello", 7 };
+    S s1 = {"Hello", 7};
 
-	cout << s1.text << endl;
+    cout << s1.text << endl;
 
-	struct {
-		string text;
-		int id;
-	} r1 = { "Hello", 7 }, r2 = { "Hi", 9 };
+    struct {
+        string text;
+        int id;
+    } r1 = {"Hello", 7}, r2 = {"Hi", 9};
 
 
-	cout << r1.text << endl;
-	cout << r2.text << endl;
+    cout << r1.text << endl;
+    cout << r2.text << endl;
 
-	vector<string> strings;
-	strings.push_back("One");
-	strings.push_back("Two");
-	strings.push_back("Three");
+    vector<string> strings;
+    strings.push_back("One");
+    strings.push_back("Two");
+    strings.push_back("Three");
 
-	return 0;
+    return 0;
 }

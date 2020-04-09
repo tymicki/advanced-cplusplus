1aadee6774ba799844d5029c7812036533c54a25
jt <jt>
2020-04-09 11:27:18 +0200

    format

--- TemplateFunctionsandTypeInference/src/TemplateFunctionsAndTypeInference.cpp
+++ TemplateFunctionsandTypeInference/src/TemplateFunctionsAndTypeInference.cpp
@@ -13,28 +13,28 @@ using namespace std;
 
 template<class T>
 void print(T n) {
-	cout << "Template version: "<< n << endl;
+    cout << "Template version: " << n << endl;
 }
 
 void print(int value) {
-	cout << "Non-template version: " << value << endl;
+    cout << "Non-template version: " << value << endl;
 }
 
 template<class T>
 void show() {
-	cout << T() << endl;
+    cout << T() << endl;
 }
 
 int main() {
-	print<string>("Hello");
-	print<int>(5);
+    print<string>("Hello");
+    print<int>(5);
 
-	print("Hi there");
+    print("Hi there");
 
-	print(5);
-	print<>(6);
+    print(5);
+    print<>(6);
 
-	show<double>();
+    show<double>();
 
-	return 0;
+    return 0;
 }

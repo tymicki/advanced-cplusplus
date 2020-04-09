c7cfec3e6c2f746e2be2a70e2e3911b0c5a49ae2
jt <jt>
2020-04-09 12:06:01 +0200

    format

--- AbstractClasses/src/AbstractClasses.cpp
+++ AbstractClasses/src/AbstractClasses.cpp
@@ -7,63 +7,64 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
 
 class Animal {
 public:
-	virtual void run()=0;
-	virtual void speak() = 0;
+    virtual void run() = 0;
+
+    virtual void speak() = 0;
 };
 
-class Dog: public Animal {
+class Dog : public Animal {
 public:
-	virtual void speak() {
-		cout << "Woof!" << endl;
-	}
+    virtual void speak() {
+        cout << "Woof!" << endl;
+    }
 };
 
-class Labrador: public Dog {
+class Labrador : public Dog {
 public:
-	Labrador() {
-		cout << "new labrador" << endl;
-	}
-	virtual void run() {
-		cout << "Labrador running" << endl;
-	}
+    Labrador() {
+        cout << "new labrador" << endl;
+    }
+
+    virtual void run() {
+        cout << "Labrador running" << endl;
+    }
 };
 
-class Husky: public Dog
-{
+class Husky : public Dog {
 public:
-	Husky()
-	{
-		cout << "new husky" << endl;
-	}
-	virtual void run()
-	{
-		cout << "Husky running" << endl;
-	}
+    Husky() {
+        cout << "new husky" << endl;
+    }
+
+    virtual void run() {
+        cout << "Husky running" << endl;
+    }
 };
 
 void test(Animal &a) {
-	a.run();
+    a.run();
 }
 
 int main() {
 
-	Labrador lab;
-	lab.run();
-	lab.speak();
+    Labrador lab;
+    lab.run();
+    lab.speak();
 
-	Animal *animals[5];
-	animals[0] = &lab;
+    Animal *animals[5];
+    animals[0] = &lab;
 
-	animals[0]->speak();
+    animals[0]->speak();
 
-	Husky husky;
+    Husky husky;
 
-	test(lab);
-	test(husky);
+    test(lab);
+    test(husky);
 
-	return 0;
+    return 0;
 }

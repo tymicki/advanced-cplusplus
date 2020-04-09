5f17d5e621338cfe25f2b2dcad20f19ccae0387b
jt <jt>
2020-04-09 11:12:41 +0200

    format

--- TemplateClasses/src/Template Classes.cpp
+++ TemplateClasses/src/Template Classes.cpp
@@ -8,31 +8,32 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 template<class T, class K>
 class Test {
 private:
-	T obj;
-	K id;
+    T obj;
+    K id;
 public:
-	Test(T obj, K m_id) {
-		this->obj = obj;
-		this->id = m_id;
-	}
+    Test(T obj, K m_id) {
+        this->obj = obj;
+        this->id = m_id;
+    }
 
-	void print() {
-		cout << obj << ": " << id << endl;
-	}
+    void print() {
+        cout << obj << ": " << id << endl;
+    }
 };
 
 int main() {
 
-	Test<string, int> test1("Hello", 5);
-	test1.print();
+    Test<string, int> test1("Hello", 5);
+    test1.print();
 
-	Test<string, double> test2("Hello again", 4.0);
-	test2.print();
+    Test<string, double> test2("Hello again", 4.0);
+    test2.print();
 
-	return 0;
+    return 0;
 }

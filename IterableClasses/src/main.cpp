78849b6167e886404d763fd11dd70a7bddfd4dfc
jt <jt>
2020-04-09 13:30:54 +0200

    format

--- IterableClasses/src/main.cpp
+++ IterableClasses/src/main.cpp
@@ -9,29 +9,28 @@
 #include <iostream>
 #include <string>
 #include "ring.h"
+
 using namespace std;
 
 int main() {
 
-	ring<string> textring(3);
+    ring<string> textring(3);
 
-	textring.add("one");
-	textring.add("two");
-	textring.add("three");
+    textring.add("one");
+    textring.add("two");
+    textring.add("three");
 
-	// C++ 98 style
-	for(ring<string>::iterator it = textring.begin(); it != textring.end(); it++)
-	{
-		cout << *it << endl;
-	}
+    // C++ 98 style
+    for (ring<string>::iterator it = textring.begin(); it != textring.end(); it++) {
+        cout << *it << endl;
+    }
 
-	cout << endl;
+    cout << endl;
 
-	// C++ 11 style
-	for(auto val : textring)
-	{
-		cout << val << endl;
-	}
+    // C++ 11 style
+    for (auto val : textring) {
+        cout << val << endl;
+    }
 
-	return 0;
+    return 0;
 }
--- IterableClasses/src/ring.h
+++ IterableClasses/src/ring.h
@@ -16,92 +16,92 @@ template<class T>
 class ring {
 
 private:
-	int m_pos;
-	int m_size;
-	T *m_values;
+    int m_pos;
+    int m_size;
+    T *m_values;
 
 public:
-	class iterator;
+    class iterator;
 
 public:
-	ring(int size) :
-			m_pos(0), m_size(size), m_values(nullptr) {
-		m_values = new T[size];
-	}
+    ring(int size) :
+            m_pos(0), m_size(size), m_values(nullptr) {
+        m_values = new T[size];
+    }
 
-	~ring() {
-		delete[] m_values;
-	}
+    ~ring() {
+        delete[] m_values;
+    }
 
-	int size() {
-		return m_size;
-	}
+    int size() {
+        return m_size;
+    }
 
-	iterator begin() {
-		return iterator(0, *this);
-	}
+    iterator begin() {
+        return iterator(0, *this);
+    }
 
-	iterator end() {
-		return iterator(m_size, *this);
-	}
+    iterator end() {
+        return iterator(m_size, *this);
+    }
 
-	void add(T value) {
-		m_values[m_pos++] = value;
+    void add(T value) {
+        m_values[m_pos++] = value;
 
-		if (m_pos == m_size) {
-			m_pos = 0;
-		}
-	}
+        if (m_pos == m_size) {
+            m_pos = 0;
+        }
+    }
 
-	T &get(int pos) {
-		return m_values[pos];
-	}
+    T &get(int pos) {
+        return m_values[pos];
+    }
 };
 
 template<class T>
 class ring<T>::iterator {
 private:
-	int m_pos;
-	ring &m_ring;
+    int m_pos;
+    ring &m_ring;
 public:
-	iterator(int pos, ring &ring_ref) :
-			m_pos(pos), m_ring(ring_ref) {
+    iterator(int pos, ring &ring_ref) :
+            m_pos(pos), m_ring(ring_ref) {
 
-	}
+    }
 
-	//Post fix
-	/*
-	 * This differs from what's in at least the original
-	 * version of the tutorial video. Really we should
-	 * return a version of the iterator as it was
-	 * before it was modified, for consistency with
-	 * the usual behaviour of the postfix operator.
-	 */
-	iterator operator++(int) {
+    //Post fix
+    /*
+     * This differs from what's in at least the original
+     * version of the tutorial video. Really we should
+     * return a version of the iterator as it was
+     * before it was modified, for consistency with
+     * the usual behaviour of the postfix operator.
+     */
+    iterator operator++(int) {
 
-		iterator old = *this;
-		++(*this);
-		return old;
-	}
+        iterator old = *this;
+        ++(*this);
+        return old;
+    }
 
-	//prefix
-	iterator& operator++() {
-		++m_pos;
+    //prefix
+    iterator &operator++() {
+        ++m_pos;
 
-		return *this;
-	}
+        return *this;
+    }
 
-	T& operator*() {
-		return m_ring.get(m_pos);
-	}
+    T &operator*() {
+        return m_ring.get(m_pos);
+    }
 
-	bool operator==(const iterator &other) const {
-		return m_pos == other.m_pos;
-	}
+    bool operator==(const iterator &other) const {
+        return m_pos == other.m_pos;
+    }
 
-	bool operator!=(const iterator &other) const {
-		return !(*this == other);
-	}
+    bool operator!=(const iterator &other) const {
+        return !(*this == other);
+    }
 
 };
 

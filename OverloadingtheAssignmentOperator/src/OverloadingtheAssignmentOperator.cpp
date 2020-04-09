c35c5243438a2f62efd6447c5d792d283ab0dee6
jt <jt>
2020-04-09 08:12:58 +0200

    format

--- OverloadingtheAssignmentOperator/src/OverloadingtheAssignmentOperator.cpp
+++ OverloadingtheAssignmentOperator/src/OverloadingtheAssignmentOperator.cpp
@@ -11,63 +11,58 @@
 
 using namespace std;
 
-class Test
-{
-	int id; 
-	string name;
+class Test {
+    int id;
+    string name;
 
 public:
-	Test() : id(0), name("")
-	{
-		
-	}
+    Test() : id(0), name("") {
 
-	Test(int id, string name) : id(id), name(name)
-	{
-		
-	}
+    }
 
-	Test(const Test& other)
-	{
-		cout << "Copy constructor running" << endl;
-		this->id = other.id;
-		this->name = other.name;
-	}
+    Test(int id, string name) : id(id), name(name) {
 
-	const Test& operator=(const Test &other) {
-		cout << "Assignment running" << endl;
-		this->id = other.id;
-		this->name = other.name;
-		return *this;
-	}
-	
-	void print() const
-	{
-		cout << id << ": " << name << endl;
-	}
+    }
+
+    Test(const Test &other) {
+        cout << "Copy constructor running" << endl;
+        this->id = other.id;
+        this->name = other.name;
+    }
+
+    const Test &operator=(const Test &other) {
+        cout << "Assignment running" << endl;
+        this->id = other.id;
+        this->name = other.name;
+        return *this;
+    }
+
+    void print() const {
+        cout << id << ": " << name << endl;
+    }
 };
 
 int main() {
 
-	Test test1(10, "Mike");
-	cout << "Print test1: " << flush;
-	test1.print();
+    Test test1(10, "Mike");
+    cout << "Print test1: " << flush;
+    test1.print();
 
-	Test test2(20, "Bob");
+    Test test2(20, "Bob");
 
-	test2 = test1;
-	cout << "Print test2: " << flush;
-	test2.print();
+    test2 = test1;
+    cout << "Print test2: " << flush;
+    test2.print();
 
-	Test test3;
-	test3 = test2;
-	cout << "Print test3: " << flush;
-	test3.print();
+    Test test3;
+    test3 = test2;
+    cout << "Print test3: " << flush;
+    test3.print();
 
-	cout << endl;
+    cout << endl;
 
-	// Copy initialization. 
-	Test test4 = test1;
-	test4.print();
-	return 0;
+    // Copy initialization.
+    Test test4 = test1;
+    test4.print();
+    return 0;
 }

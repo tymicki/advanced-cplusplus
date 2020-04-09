1496a13cd0e3e1b6792c6ec7f274fc9c0fadaa67
jt <jt>
2020-04-09 15:01:51 +0200

    format

--- InitializationInC11/src/main.cpp
+++ InitializationInC11/src/main.cpp
@@ -8,42 +8,43 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
 
-	int value { 5 };
-	cout << value << endl;
+    int value{5};
+    cout << value << endl;
 
-	string text { "Hello" };
-	cout << text << endl;
+    string text{"Hello"};
+    cout << text << endl;
 
-	int numbers[] { 1, 2, 4 };
-	cout << numbers[1] << endl;
+    int numbers[]{1, 2, 4};
+    cout << numbers[1] << endl;
 
-	int *pInts = new int[3] { 1, 2, 3 };
-	cout << pInts[1] << endl;
-	delete[] pInts;
+    int *pInts = new int[3]{1, 2, 3};
+    cout << pInts[1] << endl;
+    delete[] pInts;
 
-	int value1 { };
-	cout << value1 << endl;
+    int value1{};
+    cout << value1 << endl;
 
-	int *pSomething { }; // equivalent to int *pSomething = nullptr;
+    int *pSomething{}; // equivalent to int *pSomething = nullptr;
 
-	cout << pSomething << endl;
+    cout << pSomething << endl;
 
-	int numbers1[5] { };
-	cout << numbers1[1] << endl;
+    int numbers1[5]{};
+    cout << numbers1[1] << endl;
 
-	struct {
-		int value;
-		string text;
-	} s1{5, "Hi"};
+    struct {
+        int value;
+        string text;
+    } s1{5, "Hi"};
 
-	cout << s1.text << endl;
+    cout << s1.text << endl;
 
-	vector<string> strings{"one", "two", "three"};
-	cout << strings[2] << endl;
+    vector<string> strings{"one", "two", "three"};
+    cout << strings[2] << endl;
 
-	return 0;
+    return 0;
 }

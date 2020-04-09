615c687be4abb239d0da3e299890e83bf9dbcb50
jt <jt>
2020-04-09 11:23:42 +0200

    format

--- TemplateFunctions1/src/Template Functions 1.cpp
+++ TemplateFunctions1/src/Template Functions 1.cpp
@@ -8,18 +8,19 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 template<class T>
 void print(T n) {
-	cout << n << endl;
+    cout << n << endl;
 }
 
 int main() {
-	print<string>("Hello");
-	print<int>(5);
+    print<string>("Hello");
+    print<int>(5);
 
-	print("Hi there");
+    print("Hi there");
 
-	return 0;
+    return 0;
 }

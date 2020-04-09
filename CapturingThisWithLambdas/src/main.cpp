ffa3141217d72676bcb867dd0eb50ec842e5c2d2
jt <jt>
2020-04-09 16:10:57 +0200

    format

--- CapturingThisWithLambdas/src/main.cpp
+++ CapturingThisWithLambdas/src/main.cpp
@@ -4,29 +4,29 @@ using namespace std;
 
 class Test {
 private:
-	int one { 1 };
-	int two { 2 };
+    int one{1};
+    int two{2};
 
 public:
-	void run() {
-		int three { 3 };
-		int four { 4 };
+    void run() {
+        int three{3};
+        int four{4};
 
-		auto pLambda = [&, this]() {
-			one = 111;
-			cout << one << endl;
-			cout << two << endl;
-			cout << three << endl;
-			cout << four << endl;
-		};
-		pLambda();
-	}
+        auto pLambda = [&, this]() {
+            one = 111;
+            cout << one << endl;
+            cout << two << endl;
+            cout << three << endl;
+            cout << four << endl;
+        };
+        pLambda();
+    }
 
 };
 
 int main() {
-	Test test;
-	test.run();
+    Test test;
+    test.run();
 
-	return 0;
+    return 0;
 }

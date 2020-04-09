ff5dd203493bd3fe7b39aa15c661875a2bdbd7ce
jt <jt>
2020-04-09 17:04:18 +0200

    format

--- LValueReferences/src/LValueReferences.cpp
+++ LValueReferences/src/LValueReferences.cpp
@@ -9,80 +9,81 @@
 #include <iostream>
 #include <vector>
 #include <memory.h>
+
 using namespace std;
 
 class Test {
 private:
-	static const int SIZE = 100;
+    static const int SIZE = 100;
 
-	int *_pBuffer;
+    int *_pBuffer;
 
 public:
-	Test() {
-		cout << "constructor" << endl;
-		_pBuffer = new int[SIZE] { };
-	}
+    Test() {
+        cout << "constructor" << endl;
+        _pBuffer = new int[SIZE]{};
+    }
 
-	Test(int i) {
-		cout << "parameterized constructor" << endl;
-		_pBuffer = new int[SIZE] { };
+    Test(int i) {
+        cout << "parameterized constructor" << endl;
+        _pBuffer = new int[SIZE]{};
 
-		for (int i = 0; i < SIZE; i++) {
-			_pBuffer[i] = 7 * i;
-		}
-	}
+        for (int i = 0; i < SIZE; i++) {
+            _pBuffer[i] = 7 * i;
+        }
+    }
 
-	Test(const Test &other) {
-		cout << "copy constructor" << endl;
+    Test(const Test &other) {
+        cout << "copy constructor" << endl;
 
-		_pBuffer = new int[SIZE] { };
+        _pBuffer = new int[SIZE]{};
 
-		memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
-	}
+        memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
+    }
 
-	Test &operator=(const Test &other) {
-		cout << "assignment" << endl;
-		_pBuffer = new int[SIZE] { };
+    Test &operator=(const Test &other) {
+        cout << "assignment" << endl;
+        _pBuffer = new int[SIZE]{};
 
-		memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
-		return *this;
-	}
+        memcpy(_pBuffer, other._pBuffer, SIZE * sizeof(int));
+        return *this;
+    }
 
-	~Test() {
-		cout << "destructor" << endl;
+    ~Test() {
+        cout << "destructor" << endl;
 
-		delete[] _pBuffer;
-	}
+        delete[] _pBuffer;
+    }
 
-	friend ostream &operator<<(ostream &out, const Test &test);
+    friend ostream &operator<<(ostream &out, const Test &test);
 };
 
 ostream &operator<<(ostream &out, const Test &test) {
-	out << "Hello from test";
-	return out;
+    out << "Hello from test";
+    return out;
 }
 
 Test getTest() {
-	return Test();
+    return Test();
 }
 
 int main() {
 
-	Test test1 = getTest();
+    Test test1 = getTest();
 
-	cout << test1 << endl;
+    cout << test1 << endl;
 
-	vector<Test> vec;
-	vec.push_back(Test());
+    vector<Test> vec;
+    vec.push_back(Test());
 
-	Test &rTest1 = test1;
+    Test &rTest1 = test1;
 
-	//Test &rTest2 = getTest();
+    //Test &rTest2 = getTest();
 
-	const Test &rTest2 = getTest();
+    const Test &rTest2 = getTest();
 
-	Test test2(Test(1));
+    Test test2(Test(1));
 
 
-	return 0;
+    return 0;
 }

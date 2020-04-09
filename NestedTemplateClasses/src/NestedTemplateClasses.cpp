ca6aa3c8a3884cc86ae54987105dc9604d9bb572
jt <jt>
2020-04-09 12:50:32 +0200

    format

--- NestedTemplateClasses/src/NestedTemplateClasses.cpp
+++ NestedTemplateClasses/src/NestedTemplateClasses.cpp
@@ -9,25 +9,25 @@
 #include <iostream>
 #include <string>
 #include "ring.h"
+
 using namespace std;
 
 int main() {
 
-	ring<string>::iterator it;
-	it.print();
+    ring<string>::iterator it;
+    it.print();
 
-	ring<string> textRing(3);
-	
-	textRing.add("one");
-	textRing.add("two");
-	textRing.add("three");
-	textRing.add("four");
+    ring<string> textRing(3);
+
+    textRing.add("one");
+    textRing.add("two");
+    textRing.add("three");
+    textRing.add("four");
 
 
-	for(auto i =0; i < textRing.size(); i++)
-	{
-		cout << textRing.get(i) << endl;
-	}
+    for (auto i = 0; i < textRing.size(); i++) {
+        cout << textRing.get(i) << endl;
+    }
 
-	return 0;
+    return 0;
 }

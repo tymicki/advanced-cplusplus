e55bfc64973b4ca9198665598d5e7f85a5f57efa
jt <jt>
2020-04-09 15:41:58 +0200

    format

--- LambdaParametersAndReturnTypes/src/LambdaParametersAndReturnTypes.cpp
+++ LambdaParametersAndReturnTypes/src/LambdaParametersAndReturnTypes.cpp
@@ -21,25 +21,24 @@ void runDivide(double (*divide)(double a, double b)) {
     cout << rval << endl;
 }
 
-int main()
-{
-    auto pGreet = [](string name) { cout << "Hello " << name << endl;};
-    
+int main() {
+    auto pGreet = [](string name) { cout << "Hello " << name << endl; };
+
     pGreet("Mike");
-    
+
     testGreet(pGreet);
-    
+
     auto pDivide = [](double a, double b) -> double {
-        if(b == 0.0) {
+        if (b == 0.0) {
             return 0;
         }
-        return a/b;
+        return a / b;
     };
-    
+
     cout << pDivide(10.0, 5.0) << endl;
     cout << pDivide(10.0, 0.0) << endl;
-    
+
     runDivide(pDivide);
-    
+
     return 0;
 }

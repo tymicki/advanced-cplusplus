e3d1317a9cce3a08f6df2f269331465fe72669d3
jt <jt>
2020-04-09 12:13:34 +0200

    format

--- Functors/src/Functors.cpp
+++ Functors/src/Functors.cpp
@@ -10,48 +10,41 @@
 
 using namespace std;
 
-struct Test
-{
-	virtual bool operator()(string &text) = 0;
+struct Test {
+    virtual bool operator()(string &text) = 0;
 };
 
-struct MatchTest : Test 
-{
-	virtual bool operator()(string &text){
-		return text == "lion";
-	}
+struct MatchTest : Test {
+    virtual bool operator()(string &text) {
+        return text == "lion";
+    }
 };
 
-struct MyMatchTest : Test
-{
-	virtual bool operator()(string &text) {
-		return text == "text";
-	}
+struct MyMatchTest : Test {
+    virtual bool operator()(string &text) {
+        return text == "text";
+    }
 };
 
-void check(string text, Test &t)
-{
-	if(t(text))
-	{
-		cout << "Text matches!" << endl;
-	}
-	else
-	{
-		cout << "No match." << endl;
-	}
+void check(string text, Test &t) {
+    if (t(text)) {
+        cout << "Text matches!" << endl;
+    } else {
+        cout << "No match." << endl;
+    }
 }
 
 int main() {
-	
-	MatchTest pred;
-	string value = "lion";
-	
-	cout << pred(value) << endl;
 
-	check(value, pred);
+    MatchTest pred;
+    string value = "lion";
 
-	MyMatchTest myPred;
-	check(value, myPred);
+    cout << pred(value) << endl;
 
-	return 0;
+    check(value, pred);
+
+    MyMatchTest myPred;
+    check(value, myPred);
+
+    return 0;
 }

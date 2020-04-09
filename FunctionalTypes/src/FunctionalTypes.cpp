4d0c7e277a7d257d0c512c8736ebc00d69207f02
jt <jt>
2020-04-09 16:14:12 +0200

    format

--- FunctionalTypes/src/FunctionalTypes.cpp
+++ FunctionalTypes/src/FunctionalTypes.cpp
@@ -24,34 +24,34 @@ struct Check {
     }
 } check1;
 
-void run(function<bool(string&)> check) {
+void run(function<bool(string &)> check) {
     string test = "stars";
     cout << check(test) << endl;
 }
 
 int main() {
-    
-    int size = 5; 
+
+    int size = 5;
     vector<string> vec{"one", "two", "three"};
-    
-    auto lambda = [size](string test) {return test.size() == size;};
-    
+
+    auto lambda = [size](string test) { return test.size() == size; };
+
     auto count = count_if(vec.begin(), vec.end(), lambda);
-        
+
     cout << count << endl;
-    
+
     count = count_if(vec.begin(), vec.end(), check);
     cout << count << endl;
-    
+
     count = count_if(vec.begin(), vec.end(), check1);
     cout << count << endl;
-    
+
     run(lambda);
     run(check1);
     run(check);
-    
-    function<int(int, int)> add = [](int one, int two) { return one + two;};
+
+    function<int(int, int)> add = [](int one, int two) { return one + two; };
     cout << add(7, 3) << endl;
-    
+
     return 0;
 }
